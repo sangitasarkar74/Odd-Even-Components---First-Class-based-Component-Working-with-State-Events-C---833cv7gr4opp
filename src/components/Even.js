2b7a339e2d77e653dfa8f72c02c717912992f60e
sangitasarkar74 <sangitasarkar@gmail.com>
2023-02-06 16:48:26 +0530

    changes done

--- src/components/Even.js
+++ src/components/Even.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 class Even extends React.Component {
+  componentWillUnmount() {
+    console.log("Even is unmounted");
+  }
   render() {
     return <div id="even">I am even</div>;
   }
 }
+
 export default Even;
--- src/components/Odd.js
+++ src/components/Odd.js
@@ -1,7 +1,12 @@
 import React from "react";
+
 class Odd extends React.Component {
+  componentWillUnmount() {
+    console.log("Odd is unmounted");
+  }
   render() {
     return <div id="odd">I am odd</div>;
   }
 }
+
 export default Odd;
